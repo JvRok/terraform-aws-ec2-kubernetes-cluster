4a4b3d847f9db7b0c9531374dbcec505c37f46ab
Ian Smith <jvonrochow@gmail.com>
2022-07-26 03:31:40 +1000

    fixed calico install

--- modules/controller-node/bootstrap/kubernetes-master-setup.sh
+++ modules/controller-node/bootstrap/kubernetes-master-setup.sh
@@ -23,26 +23,26 @@ dnf install -y grubby && \
 #ContainerD Install
 dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 dnf -y install containerd
-containerd config default | sudo tee /etc/containerd/config.toml
+containerd config default | tee /etc/containerd/config.toml
 sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
 systemctl restart containerd
 
 #Network settings kubernetes requires
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+cat <<EOF | tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
 
-sudo modprobe overlay
-sudo modprobe br_netfilter
+modprobe overlay
+modprobe br_netfilter
 
 # sysctl params required by setup, params persist across reboots
 # Currently seeing issues here in centos9 with interpreting, look at this later.
-cat < /etc/sysctl.d/k8s.conf
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
-
+EOF
 
 # Apply sysctl params without reboot
 sysctl --system
@@ -52,7 +52,7 @@ sysctl --system
 #systemctl stop firewalld
 
 
-cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
@@ -64,20 +64,20 @@ EOF
 
 # Already disabled it myself, these comments below are from kubernetes docs
 # Set SELinux in permissive mode (effectively disabling it)
-#sudo setenforce 0
-#sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+#setenforce 0
+#sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
-sudo dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 
-sudo systemctl enable --now kubelet
+systemctl enable --now kubelet
 
 
 #Now its time for pod racing
-sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+kubeadm init --pod-network-cidr=192.168.0.0/16
 
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+mkdir -p /root/.kube
+cp -i /etc/kubernetes/admin.conf /root/.kube/config
+chown $(id -u):$(id -g) /root/.kube/config
 
 kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml
 kubectl create -f https://projectcalico.docs.tigera.io/manifests/custom-resources.yaml
